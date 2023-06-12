8d4869f4e5f86f5028b74e4efc75cb2e8e8d3790
Danillodn <danillodiasnascimento@gmail.com>
2023-06-12 16:04:48 -0300

    added taskList

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 // components
 import Header from "./components/Header";
@@ -13,17 +13,20 @@ import styles from './App.module.css'
 import { ITask } from '../src/interfaces/Task'
 
 function App() {
+
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+
   return (
     <div>
       <Header />
       <main className={styles.main}>
         <div>
           <h2>O que voce vai fazer?</h2>
-          <TaskForm btnText="Criar tarefa" />
+          <TaskForm btnText="Criar tarefa" taskList={taskList} setTaskList={setTaskList}/>
         </div>
         <div>
           <h2>Suas tarefas</h2>
-          <TaskList />
+          <TaskList taskList={taskList} />
         </div>
       </main>
       <Footer />
