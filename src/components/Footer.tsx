e928be75655c102c44c37f112c354e1129578903
Danillodn <danillodiasnascimento@gmail.com>
2023-06-13 15:40:40 -0300

    refactoring  footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,10 +6,10 @@ import styles from './Footer.module.css'
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-        <p>
-          <span>React With TS</span> @2023
-        </p>
-      </footer>
+      <p>
+        <span>React With TS</span> @2023
+      </p>
+    </footer>
   )
 }
 
